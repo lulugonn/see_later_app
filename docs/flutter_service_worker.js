0254d37213d532ccbe88bfdfed8b31db261de4ff
Luana <luanagoncalves@dcc.ufrj.br>
2024-02-27 02:21:27 -0300

    feat(login):  try deploy

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -22,8 +22,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "8955a9667edc24e6814316468d1a9c87",
-"/": "8955a9667edc24e6814316468d1a9c87",
+"index.html": "90cf29e567d72719856feb2715190a50",
+"/": "90cf29e567d72719856feb2715190a50",
 "main.dart.js": "e1bffba29bf6fcff00b6c8d1164e0969",
 "manifest.json": "db000be743a7188e1d78217689a4aa32",
 "version.json": "f7530eef61b11664155f6ec665fe0998"
